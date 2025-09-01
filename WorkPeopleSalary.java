994c6a9ba60320af9ed5a3b1898ccdfbfe012fd2
Александр Сизов <115085855+AxelFoleY11@users.noreply.github.com>
2025-09-01 10:07:12 +0300

    Delete WorkPeopleSalary.java

--- WorkPeopleSalary.java
+++
@@ -1,9 +0,0 @@
-public class WorkPeopleSalary {
-    public static void main(String[] args) {
-
-        int MaxSalary = 5000;
-        int MedianSalary = 4000;
-        int MinimalSalary = 3000;
-
-    }
-}
